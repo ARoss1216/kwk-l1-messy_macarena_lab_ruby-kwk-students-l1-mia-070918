51daefc42675f7468161db1d9cccecf820d8f45d
ARoss1216 <a.ross24852@gmail.com>
2018-07-09 17:50:06 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -7,7 +7,7 @@ puts "4. Turn your left palm up."
 puts "5. Place your right hand on your left shoulder."
 puts "6. Place your left hand on your right shoulder."
 puts "7. Bring your right hand up and around to the back of your head."
-puts "8. Bring your left hand up and around to the back of your head."
+puts "8. Bring your left hand up and around to the back your head."
 
 # Fix The Dance Instructions Below
 # Every line should have one instruction using puts.
