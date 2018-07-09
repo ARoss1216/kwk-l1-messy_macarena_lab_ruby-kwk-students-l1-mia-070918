0f4d6fff1f5edd7d6cc7edf679f835bfed805e18
ARoss1216 <a.ross24852@gmail.com>
2018-07-09 17:44:51 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -2,7 +2,7 @@ puts "How To Do The Macarena:"
 puts "1. Right arm out in front of you, palm facing down."
 puts "2. Left arm out in front of you, palm facing down."
 puts "3. Turn your right palm up."
-
+puts "4. 
 
 # Fix The Dance Instructions Below
 # Every line should have one instruction using puts.
